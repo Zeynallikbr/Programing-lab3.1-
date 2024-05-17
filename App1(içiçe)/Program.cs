0fc7d0262de11ff51754bbc163c88cd819462ba0
Ekber Zeynalli <zeynallikbr2@gmail.com>
2024-05-17 15:23:05 +0400

    for dongusu

---
+++ App1(içiçe)/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace App1_içiçe_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 6; a++)
+            {
+                Console.WriteLine($"a {a}");
+                for (int i = 1;  i<=3; i++)
+                {
+
+                    Console.WriteLine($"i {i}");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
